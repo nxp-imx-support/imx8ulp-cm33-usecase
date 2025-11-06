632d3e139884ef10cc9dffb22fe19f2f81205755
Jason He <jason.he_1@nxp.com>
2025-11-06 14:51:45 +0900

    use hamming(12,8) in rpmsg_ns
    Signed-off-by: Jason He <jason.he_1@nxp.com>

--- middleware/multicore/rpmsg_lite/lib/rpmsg_lite/rpmsg_ns.c
+++ middleware/multicore/rpmsg_lite/lib/rpmsg_lite/rpmsg_ns.c
@@ -34,7 +34,7 @@
 #include "rpmsg_ns.h"
 #include <stdint.h>
 
-#define RL_NS_NAME_SIZE (32)
+#define RL_NS_NAME_SIZE (64)
 
 /*!
  * struct rpmsg_ns_msg - dynamic name service announcement message
@@ -55,6 +55,78 @@ struct rpmsg_ns_msg
     uint32_t addr;
     uint32_t flags;
 } RL_PACKED_END;
+
+// Encode 8-bit data into 12-bit Hamming code (stored in a short)
+short hamming_encode_12_8(char data) {
+    short codeword = 0;
+    int d[8], p1, p2, p4, p8;
+
+    // Extract individual bits from the input data
+    for (int i = 0; i < 8; i++) {
+        d[i] = (data >> i) & 1;
+    }
+
+    // Place data bits into codeword at positions 3,5,6,7,9,10,11,12
+    if (d[0]) codeword |= (1 << 2);
+    if (d[1]) codeword |= (1 << 4);
+    if (d[2]) codeword |= (1 << 5);
+    if (d[3]) codeword |= (1 << 6);
+    if (d[4]) codeword |= (1 << 8);
+    if (d[5]) codeword |= (1 << 9);
+    if (d[6]) codeword |= (1 << 10);
+    if (d[7]) codeword |= (1 << 11);
+
+    // Calculate parity bits for positions 1,2,4,8
+    p1 = ((codeword >> 2) & 1) ^ ((codeword >> 4) & 1) ^ ((codeword >> 6) & 1) ^ ((codeword >> 8) & 1) ^ ((codeword >> 10) & 1);
+    p2 = ((codeword >> 2) & 1) ^ ((codeword >> 5) & 1) ^ ((codeword >> 6) & 1) ^ ((codeword >> 9) & 1) ^ ((codeword >> 10) & 1);
+    p4 = ((codeword >> 4) & 1) ^ ((codeword >> 5) & 1) ^ ((codeword >> 6) & 1) ^ ((codeword >> 11) & 1);
+    p8 = ((codeword >> 8) & 1) ^ ((codeword >> 9) & 1) ^ ((codeword >> 10) & 1) ^ ((codeword >> 11) & 1);
+
+    // Set parity bits in codeword
+    if (p1) codeword |= (1 << 0);
+    if (p2) codeword |= (1 << 1);
+    if (p4) codeword |= (1 << 3);
+    if (p8) codeword |= (1 << 7);
+
+    return codeword;
+}
+
+// Decode 12-bit Hamming codeword into original 8-bit data
+char hamming_decode_12_8(short codeword, int *error_detected) {
+    // Calculate syndrome bits to detect error position
+    int s1 = ((codeword >> 0) & 1) ^ ((codeword >> 2) & 1) ^ ((codeword >> 4) & 1) ^ ((codeword >> 6) & 1) ^ ((codeword >> 8) & 1) ^ ((codeword >> 10) & 1);
+    int s2 = ((codeword >> 1) & 1) ^ ((codeword >> 2) & 1) ^ ((codeword >> 5) & 1) ^ ((codeword >> 6) & 1) ^ ((codeword >> 9) & 1) ^ ((codeword >> 10) & 1);
+    int s4 = ((codeword >> 3) & 1) ^ ((codeword >> 4) & 1) ^ ((codeword >> 5) & 1) ^ ((codeword >> 6) & 1) ^ ((codeword >> 11) & 1);
+    int s8 = ((codeword >> 7) & 1) ^ ((codeword >> 8) & 1) ^ ((codeword >> 9) & 1) ^ ((codeword >> 10) & 1) ^ ((codeword >> 11) & 1);
+
+    int error_pos = s8 * 8 + s4 * 4 + s2 * 2 + s1;
+
+	char data = 0;
+
+    // If error detected, correct the bit
+    if (error_pos != 0 && error_pos <= 12) {
+        if (error_detected != NULL) {
+			*error_detected = error_pos;
+		}
+        codeword ^= (1 << (error_pos - 1));
+    } else {
+		if (error_detected != NULL) {
+			*error_detected = 0;
+		}
+	}
+
+    // Extract original data bits from codeword
+    if ((codeword >> 2) & 1) data |= (1 << 0);
+    if ((codeword >> 4) & 1) data |= (1 << 1);
+    if ((codeword >> 5) & 1) data |= (1 << 2);
+    if ((codeword >> 6) & 1) data |= (1 << 3);
+    if ((codeword >> 8) & 1) data |= (1 << 4);
+    if ((codeword >> 9) & 1) data |= (1 << 5);
+    if ((codeword >> 10) & 1) data |= (1 << 6);
+    if ((codeword >> 11) & 1) data |= (1 << 7);
+
+    return data;
+}
 
 /*!
  * @brief
@@ -169,6 +241,7 @@ int32_t rpmsg_ns_announce(struct rpmsg_lite_instance *rpmsg_lite_dev,
                           uint32_t flags)
 {
     struct rpmsg_ns_msg ns_msg;
+    int16_t  *p_encoded_name = ns_msg.name;
 
     if (ept_name == RL_NULL)
     {
@@ -180,7 +253,12 @@ int32_t rpmsg_ns_announce(struct rpmsg_lite_instance *rpmsg_lite_dev,
         return RL_ERR_PARAM;
     }
 
-    env_strncpy(ns_msg.name, ept_name, RL_NS_NAME_SIZE);
+    for (int32_t i = 0; i < RL_NS_NAME_SIZE / 2; i++) {
+        *p_encoded_name = hamming_encode_12_8(ept_name[i]);
+        p_encoded_name++;
+    }
+
+    // env_strncpy(ns_msg.name, ept_name, RL_NS_NAME_SIZE);
     ns_msg.flags = flags;
     ns_msg.addr  = new_ept->addr;
 
